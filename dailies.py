2cc96fac1d73ae9522ee70f8bd230ec25a115741
Maggie Wilson <maggieowilson@gmail.com>
2023-02-24 16:24:24 -0800

    day 6 - easy day!

--- dailies.py
+++ dailies.py
@@ -357,4 +357,26 @@ def day5(inputData):
 
 def day6(inputData):
     with open(inputData) as file:
-        lines = [line.rstrip() for line in file]
+        message = [line.rstrip() for line in file]
+    message = message[0] # reformat into single string - this input is different than the others
+
+    """
+    Part 1 - start of packet marker 
+    """
+    # first eligible start character is the fourth (index 3)
+    for endIdx in range(3, len(message)):
+        lastFour = message[endIdx-4:endIdx]
+
+        if len(set(lastFour)) == 4:
+            print(f'Part 1: {lastFour} found after scanning {endIdx} characters')
+            break
+
+    """
+    Part 2 - start of message market 
+    """
+    for endIdx in range(13, len(message)):
+        lastFour = message[endIdx-14:endIdx]
+
+        if len(set(lastFour)) == 14:
+            print(f'Part 2: {lastFour} found after scanning {endIdx} characters')
+            break
--- main.py
+++ main.py
@@ -4,4 +4,5 @@ from dailies import *
 # day2('data/day2input.txt')
 # day3('data/day3input.txt')
 # day4('data/day4input.txt')
-day5('data/day5input.txt')
+# day5('data/day5input.txt')
+day6('data/day6input.txt')
